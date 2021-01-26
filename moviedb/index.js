8ee99bb79e8e52bf64833838d1111f5c4654528e
Kshitij Agrawal <kshitij.agrawal2010@gmail.com>
2021-01-26 15:09:09 +0530

    feat: added docker image, updated config

--- moviedb/index.js
+++ moviedb/index.js
@@ -1,28 +1,27 @@
-const { moviedbApikey } = require('../config.json');
-const { MovieDb } = require('moviedb-promise');
-const moviedb = new MovieDb(moviedbApikey);
-
+const { moviedbApikey } = require('../config.js')
+const { MovieDb } = require('moviedb-promise')
+const moviedb = new MovieDb(moviedbApikey)
 
 const findMovie = async title => {
-	// Equivalant to { query: title }
-	const res = await moviedb.searchMovie(title);
-	return res;
-};
+  // Equivalant to { query: title }
+  const res = await moviedb.searchMovie(title)
+  return res
+}
 
 const configuration = async () => {
-	const res = await moviedb.configuration();
-	return res;
-};
+  const res = await moviedb.configuration()
+  return res
+}
 
 const getImageUrl = async path => {
-	const res = await moviedb.configuration();
-	// todo: make below line more readable.
-	return `${res.images.base_url}${res.images.poster_sizes[4]}/${path}`;
-};
+  const res = await moviedb.configuration()
+  // todo: make below line more readable.
+  return `${res.images.base_url}${res.images.poster_sizes[4]}/${path}`
+}
 
 module.exports = {
-	moviedb: moviedb,
-	findMovie: findMovie,
-	configuration: configuration,
-	getImageUrl: getImageUrl,
-};
+  moviedb: moviedb,
+  findMovie: findMovie,
+  configuration: configuration,
+  getImageUrl: getImageUrl,
+}

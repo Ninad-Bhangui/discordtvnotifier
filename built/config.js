09c31736b9c49028b16af669367319e250da02f6
Ninad <b.ninad50@gmail.com>
2021-05-07 09:03:28 +0530

    add compiled js files to git so heroku can run them

---
+++ built/config.js
@@ -0,0 +1,7 @@
+'use strict'
+console.log(`Env: ${process.env['DISCORD_TOKEN']}`)
+module.exports = {
+  prefix: process.env['DISCORD_PREFIX'],
+  token: process.env['DISCORD_TOKEN'],
+  moviedbApikey: process.env['MOVIEDB_API_KEY'],
+}
